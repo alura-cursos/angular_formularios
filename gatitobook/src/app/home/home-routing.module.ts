9bed12db3ea4223e58cc70897d239cec4b045a62
Alvaro <alvaro.camilloneto@gmail.com>
2020-12-02 17:40:54 -0300

    Aula 1.8

--- gatitobook/src/app/home/home-routing.module.ts
+++ gatitobook/src/app/home/home-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    children: [
+      {
+        path: '',
+        component: LoginComponent,
+      },
+    ],
   },
 ];
 
