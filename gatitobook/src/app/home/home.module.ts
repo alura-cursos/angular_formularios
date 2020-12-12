dc283b2c440ff6fac001be0d3381dd964cc8cfe2
Alvaro <alvaro.camilloneto@gmail.com>
2020-12-12 17:47:35 -0300

    Aula 3.4

--- gatitobook/src/app/home/home.module.ts
+++ gatitobook/src/app/home/home.module.ts
@@ -4,13 +4,19 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MensagemModule } from '../componentes/mensagem/mensagem.module';
 import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
 @NgModule({
   declarations: [HomeComponent, LoginComponent, NovoUsuarioComponent],
-  imports: [CommonModule, HomeRoutingModule, FormsModule, MensagemModule],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    FormsModule,
+    MensagemModule,
+    ReactiveFormsModule,
+  ],
   exports: [HomeComponent],
 })
 export class HomeModule {}
--- gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NovoUsuario } from './novo-usuario';
+import { NovoUsuarioService } from './novo-usuario.service';
 
 @Component({
   selector: 'app-novo-usuario',
   templateUrl: './novo-usuario.component.html',
-  styleUrls: ['./novo-usuario.component.css']
+  styleUrls: ['./novo-usuario.component.css'],
 })
 export class NovoUsuarioComponent implements OnInit {
+  novoUsuarioForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private novoUsuarioService: NovoUsuarioService
+  ) {}
 
   ngOnInit(): void {
+    this.novoUsuarioForm = this.formBuilder.group({
+      email: [''],
+      fullName: [''],
+      userName: [''],
+      password: [''],
+    });
   }
 
+  cadastrar() {
+    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+    console.log(novoUsuario);
+  }
 }
