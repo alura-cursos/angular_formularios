ae973a1cd1b5dd9ba202ff25b1c8f2278d0b92ea
Alvaro <alvaro.camilloneto@gmail.com>
2020-12-14 18:35:33 -0300

    Aula 5.3

---
+++ gatitobook/src/app/autenticacao/usuario/usuario.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { TokenService } from '../token.service';
+import { Usuario } from './usuario';
+import jwt_decode from 'jwt-decode';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsuarioService {
+  private usuarioSubject = new BehaviorSubject<Usuario>({});
+
+  constructor(private tokenService: TokenService) {
+    if (this.tokenService.possuiToken()) {
+      this.decodificaJWT();
+    }
+  }
+
+  private decodificaJWT() {
+    const token = this.tokenService.retornaToken();
+    const usuario = jwt_decode(token) as Usuario;
+    this.usuarioSubject.next(usuario);
+  }
+
+  retornaUsuario() {
+    return this.usuarioSubject.asObservable();
+  }
+
+  salvaToken(token: string) {
+    this.tokenService.salvaToken(token);
+    this.decodificaJWT();
+  }
+
+  logout() {
+    this.tokenService.excluiToken();
+    this.usuarioSubject.next({});
+  }
+
+  estaLogado() {
+    return this.tokenService.possuiToken();
+  }
+}
