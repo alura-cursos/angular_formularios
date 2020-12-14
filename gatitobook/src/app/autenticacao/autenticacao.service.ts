50f06670deefe95d1b0c7b3b10526376ed2c9b7a
Alvaro <alvaro.camilloneto@gmail.com>
2020-12-14 18:42:19 -0300

    Aula 5.4

--- gatitobook/src/app/autenticacao/autenticacao.service.ts
+++ gatitobook/src/app/autenticacao/autenticacao.service.ts
@@ -1,17 +1,33 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { UsuarioService } from './usuario/usuario.service';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AutenticacaoService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private usuarioService: UsuarioService
+  ) {}
 
-  autenticar(usuario: string, senha: string): Observable<any> {
-    return this.httpClient.post('http://localhost:3000/user/login', {
-      userName: usuario,
-      password: senha,
-    });
+  autenticar(usuario: string, senha: string): Observable<HttpResponse<any>> {
+    return this.httpClient
+      .post(
+        'http://localhost:3000/user/login',
+        {
+          userName: usuario,
+          password: senha,
+        },
+        { observe: 'response' }
+      )
+      .pipe(
+        tap((res) => {
+          const authToken = res.headers.get('x-access-token') ?? '';
+          this.usuarioService.salvaToken(authToken);
+        })
+      );
   }
 }
