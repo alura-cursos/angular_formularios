4da55a23b383d998d9d62a6468b52a432774680f
Alvaro <alvaro.camilloneto@gmail.com>
2020-12-14 18:00:28 -0300

    Aula 5.2

---
+++ gatitobook/src/app/autenticacao/token.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+const KEY = 'token';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TokenService {
+  retornaToken() {
+    return localStorage.getItem(KEY) ?? '';
+  }
+
+  salvaToken(token: string) {
+    localStorage.setItem(KEY, token);
+  }
+
+  excluiToken() {
+    localStorage.removeItem(KEY);
+  }
+
+  possuiToken() {
+    return !!this.retornaToken();
+  }
+  
+}
